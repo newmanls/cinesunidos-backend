93d08b51de57e3207dece95feb9dd2bedc457ffb
Newman Sánchez <newmanls@pm.me>
2024-05-16 15:57:38 -0400

    improve showtimes output and add filters by date and theatre_id

--- api/views.py
+++ api/views.py
@@ -1,5 +1,6 @@
-import json
 from collections import defaultdict
+from datetime import datetime
+
 from rest_framework import permissions, viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
@@ -17,19 +18,47 @@ class MovieViewSet(viewsets.ModelViewSet):
     def showtimes(self, request, pk=None):
         movie = self.get_object()
         movie_serializer = self.get_serializer(movie)
-        showtimes = Showtime.objects.filter(movie=movie)
-        theatres = defaultdict(dict)
+        showtimes = Showtime.objects.filter(movie=movie).order_by('theatre__id', 'format')
+        theatres = []
 
+        # Query filters
+        query_date = self.request.query_params.get('date')
+        query_theatre_id = self.request.query_params.get('theatre_id')
+
+        if query_date is not None:
+            showtimes = showtimes.filter(time__date=query_date)
+        else:
+            showtimes = showtimes.filter(time__date=datetime.now().date())
+
+        if query_theatre_id is not None:
+            showtimes = showtimes.filter(theatre__id=query_theatre_id)
+
+        # Output formatting
         for showtime in showtimes:
-            theatre_id = showtime.theatre.name
-            format_id = showtime.get_format_display()
+            theatre_id = showtime.theatre.id
+            theatre_name = showtime.theatre.name
+            theatre_address = showtime.theatre.address
+            format = showtime.get_format_display()
             language = showtime.get_language_display()
+            format_full = f'{format} ({language})'
 
-            if theatre_id not in theatres:
-                theatres[theatre_id] = defaultdict(list)
+            # Check if theatre already exists in theatres list
+            theatre_exists = any(theatre['id'] == theatre_id for theatre in theatres)
 
-            theatres[theatre_id][f'{format_id} ({language})'].append(
-                    ShowtimeSerializer(showtime).data)
+            if not theatre_exists:
+                theatre_data = {
+                    'id': theatre_id,
+                    'name': theatre_name,
+                    'address': theatre_address,
+                    'formats': defaultdict(list)
+                }
+                theatres.append(theatre_data)
+
+            # Find the theatre in the list
+            theatre_data = next(theatre for theatre in theatres if theatre['id'] == theatre_id)
+
+            # Append showtime data to the formats list of the theatre
+            theatre_data['formats'][format_full].append(ShowtimeSerializer(showtime).data)
 
         data = movie_serializer.data
         data['theatres'] = theatres
